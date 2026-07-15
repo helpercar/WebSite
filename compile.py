fdbcfc0523f5f888bdd49070606a28a5b9ba6e9d
helpercar <marcomejia.hc@gmail.com>
2026-07-14 20:07:23 -0700

    fix: Fixed Pathing on compile.py

--- compile.py
+++ compile.py
@@ -11,16 +11,22 @@ def build_site():
         'blog.html': 'Blog/index.html'
     }
     
-    for template_name, output_path in pages.items():
+    for template_name, raw_output_path in pages.items():
+        output_path = os.path.normpath(raw_output_path)
+
         # Handle subfolder generation safely
         dir_name = os.path.dirname(output_path)
+
         if dir_name:
             os.makedirs(dir_name, exist_ok=True)
+
+        # Calculate Path Depth
+        if not dir_name:
+            path_base = './'
+        else:
+            depth = len(dir_name.split(os.sep))
+            path_base = "../" * depth
             
-        # Calculate folder depth to find the asset directory path
-        # For index.html -> path_base is "./"
-        # For Projects/index.html -> path_base is "../"
-        path_base = "./" if not dir_name else "../" * len(dir_name.split(os.sep))
 
         template = env.get_template(template_name)
         
